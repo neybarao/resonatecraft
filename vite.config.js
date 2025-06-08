b7bc0a3793952b78c64cf5d026ca206039ddd6fe
Ney Barão <394842+neybarao@users.noreply.github.com>
2025-06-08 19:44:21 -0400

    fix: update to use absolute URLs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/resonatecraft/',
+  base: 'https://neybarao.github.io/resonatecraft/',
   plugins: [
     react({
       jsxRuntime: 'automatic',
